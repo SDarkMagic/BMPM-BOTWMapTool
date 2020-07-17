da049cb343e8e43bccbba04e91eda6af6549f493
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-07-17 09:42:38 -0400

    Added an option to not compress output file with yaz0 as well as fixing a bug with reading non-compressed files

--- bymlMapParamEditor.py
+++ bymlMapParamEditor.py
@@ -1,6 +1,5 @@
 import oead
 import os
-import sys
 import argparse
 
 
@@ -11,6 +10,7 @@ parser.add_argument('fileIn', type=str, help="File to open and read data from.")
 parser.add_argument('termToSearch', type=str, help='Term to look through the file and find.')
 parser.add_argument('replacementTerm', help='Value to replace the original termToSearch value with.')
 parser.add_argument('--type', '-t', metavar='value', dest='type', choices=['0', '1'], help='A required value of 0 meaning an integer or 1 meaning a string to set the replacement term as.', required=True)
+parser.add_argument('--noCompression', '-nc', action='store_true', help='Add this if you would prefer the program to not compress the output file with yaz0.')
 args = parser.parse_args()
 
 
@@ -37,11 +37,12 @@ def checkCompression(fileCheck):
         uncompressedFile = oead.yaz0.decompress(fileInRead)
     else:
         print('File is not compressed with Yaz0')
-        uncompressedFile = fileCheck
+        uncompressedFile = fileInRead
     return(uncompressedFile)
 
 # Function for replacing inputted parameter with other user input
 def replaceParam(fileToOpen):
+    global fileExt
     fileDict = {}
     entryDict = {}
     paramDict = {}
@@ -52,37 +53,43 @@ def replaceParam(fileToOpen):
     for key in extractByml.keys():
         fileDict.update({key: extractByml.get(key)})
     array = fileDict.get('Objs')
-    fileWrite = open(fileName + fileExt, 'wb')
 
     for entry in array:
         exactItem = array[iterate]
         entryDict.update(exactItem)
         iterate += 1
         if (entryDict.get('!Parameters') is not None):
-            print('Found "!Parameters" value in entry from file')
+#            print('Found "!Parameters" value in entry from file')
             paramDict.update(entryDict.get('!Parameters'))
             for key in paramDict.keys():
-                print('Checking if param is the same as user input to be replaced')
+#                print('Checking if param is the same as user input to be replaced')
                 if (key == termToSearch):
                     paramDict.update({termToSearch: replacementParamType})
                     entryDict.update({'!Parameters': paramDict})
-                    print('Successfully replaced parameter')
+#                    print('Successfully replaced parameter')
         
-        print(entryDict)
+#        print(entryDict)
         objList.append(oead.byml.Hash(entryDict))
         paramDict.clear()
         entryDict.clear()
 
     fileDict.update({'Objs': objList})
-#    print(objList)
-    fileWrite.write(oead.yaz0.compress(oead.byml.to_binary(fileDict, False)))
-#    oead.yaz0.compress(fileWrite.read())
+    if (args.noCompression):
+            extList = []
+            fileExt = fileExt.lstrip('.s')
+            fileExt = ('.') + fileExt
+            fileWrite = open(fileName + fileExt, 'wb')
+            fileWrite.write(oead.byml.to_binary(fileDict, False))
+
+    else:
+        fileWrite = open(fileName + fileExt, 'wb')
+        fileWrite.write(oead.yaz0.compress(oead.byml.to_binary(fileDict, False)))
+        print("Compressing file.")
+
     fileWrite.close()
 
 
 if __name__ == '__main__':
-#    outFile.write('Objs:\n')
-#    outFile.close()
     replaceParam(openFile)
     openFile.close()
     print("done")
